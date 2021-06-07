aeebae089bdc4c8a55f2baf4ff97c9920a7c337b
Paul Stearns <celestialcoyote@gmail.com>
2021-06-07 16:06:32 -0400

    Setup webpack production build.

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Git Commands
+
+git init - Create a new git repository
+git status - View the changes to project code
+git add path/filename - Add single file to staging area
+git add . - Add all tracked files to staging area
+git commit -m "Commit comment" - Creates a new commit with files from staging area, must add comment
+git log - View recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,42 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 // Entry point -> Output file.
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 8081,
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'styles.css',
+            }),
+        ],
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'    
+                ]
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            port: 8081,
+            historyApiFallback: true
+        }
+    };
 };
